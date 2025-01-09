5619e3b9a1f2cc19e2b11fc6931efa6cce2cec42
Crstl-dm <cristalduranmercado7@gmail.com>
2025-01-09 13:01:09 +0100

    Update create_db_livres.py

--- create_db_livres.py
+++ create_db_livres.py
@@ -7,16 +7,11 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", 
-            ('L\'Étranger', 'Albert Camus', 'Roman', 10, 10))
-cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", 
-            ('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction', 15, 15))
-cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", 
-            ('Les Misérables', 'Victor Hugo', 'Drame', 5, 5))
-cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", 
-            ('1984', 'George Orwell', 'Science-fiction', 12, 12))
-cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", 
-            ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 'Fantaisie', 20, 20))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('L\'Étranger', 'Albert Camus', 'Roman', 10, 10))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction', 15, 15))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Les Misérables', 'Victor Hugo', 'Drame', 5, 5))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('1984', 'George Orwell', 'Science-fiction', 12, 12))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 'Fantaisie', 20, 20))
 
 connection.commit()
 connection.close()
