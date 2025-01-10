95e5d23af2de520db72a27851b567abe7298dc9b
Crstl-dm <cristalduranmercado7@gmail.com>
2025-01-10 15:40:35 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -16,5 +16,11 @@ cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGN
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('L\'Étranger', 'Albert Camus', 'Roman', 10, 10))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Fiction', 15, 15))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Les Misérables', 'Victor Hugo', 'Drame', 5, 5))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('1984', 'George Orwell', 'Science-fiction', 12, 12))
+cur.execute("INSERT INTO livres (titre, auteur, genre, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)", ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 'Fantaisie', 20, 20))
+
 connection.commit()
 connection.close()
